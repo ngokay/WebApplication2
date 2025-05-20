64427624bf3a21e83891ec10ae4fa81ab58e2ab7
anhnt <anhnt@epayjsc.com>
2025-05-20 16:28:22 +0700

    no message

---
+++ WebApplication2/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data.Model;
+
+namespace WebApplication2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
